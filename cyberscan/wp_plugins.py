20fc4469ee39c08700414953cb73c45626b4aefa
cyberwarn <maksim@webcadev.ru>
2026-02-18 17:52:50 +0400

    complite parser

--- cyberscan/wp_plugins.py
+++ cyberscan/wp_plugins.py
@@ -9,7 +9,7 @@ import sys
 from .header import Headers
 from .helper import CyberHelp
 from .colors import RED, RESET, BLUE, GREEN, BOLD, YELLOW
-from .core import greeting, CoreSetting
+from .core import greeting, CoreSetting, divide_line
 
 def check_wordpress(url:str) -> bool:
     """Проверяем, на WordPress сайт или нет"""
@@ -32,6 +32,7 @@ def check_wordpress(url:str) -> bool:
 
 def full_url_plugin(url:str, plugin:str) -> str:
     """Возвращает полный url до readme.md"""
+    if url[-1] == "/":url = url[:-1]
     full_url = f"{url}/wp-content/plugins/{plugin}/readme.txt"
     return full_url
 
@@ -44,11 +45,49 @@ def full_list_plugins() -> list[str]:
             list_plugins.append(line.strip())
     return list_plugins
 
+def parser_txt(response:str) -> dict[str]:
+    """Парсер readme.txt, возвращает dict с именем и версией"""
+    
+    data = {
+            "plugin_name":None,
+            "plugin_version":None
+            }
+    
+    readme = response.text
+    all_line_readme = readme.split("\n")
+    for count_line, line in enumerate(all_line_readme):
+        if count_line == 0:
+            plugin_name = line.replace("=", "").replace("\r", "").strip()
+            data["plugin_name"] = plugin_name
+        if "Stable tag: " in line:
+            plugin_version = line.split("Stable tag: ")[1].strip()
+            data["plugin_version"] = plugin_version
+
+    return data
+
 def scan_list_plugin(url:str) -> None:
+    headers = Headers()
     list_plugins = full_list_plugins()
+    number_valid_plugin = 0
+    count_plugin = 0
     for plugin in list_plugins:
+        count_plugin+=1
         full_url = full_url_plugin(url=url, plugin=plugin)
-        print(full_url)
+        #print(full_url)
+        header = headers.create_headers()
+        response = requests.get(full_url, headers=header)
+        if response.status_code == 200:
+            number_valid_plugin+=1
+            data = parser_txt(response=response)
+            plugin_name = data["plugin_name"]
+            plugin_version = data["plugin_version"]
+            print(
+                    f"|{divide_line()}\n"
+                    f"| [{number_valid_plugin}]\n"
+                    f"| {GREEN}Plugin:     {BOLD}{plugin_name}{RESET}\n"
+                    f"| {GREEN}Version:    {BOLD}{plugin_version}{RESET}"
+                    )
+        print(f"[{count_plugin}/{len(list_plugins)}]  {plugin}", end="\r")
 
 
 def scanWordPressPlugins(url:str):
