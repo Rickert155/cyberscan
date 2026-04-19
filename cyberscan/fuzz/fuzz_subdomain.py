05a573fb8a40b154f73406c46f36886029a91903
cyberwarn <maksim@webcadev.ru>
2026-04-19 14:52:40 +0400

    complite base version fuzz_subdomain

--- cyberscan/fuzz/fuzz_subdomain.py
+++ cyberscan/fuzz/fuzz_subdomain.py
@@ -28,6 +28,10 @@ def get_wordlist(wordlist_path:str) -> set:
     except Exception as err:
         sys.exit(f"{RED}{err}{RESET}")
 
+def recording_subdomain(subdomain:str, path_file:str) -> None:
+    with open(path_file, "a+") as file:
+        file.write(f"{subdomain}\n")
+
 def check_subdomain(subdomain:str) -> list[bool, str]:
     status_subdomain = True 
     try:
@@ -47,8 +51,14 @@ def fuzz_subdomains(url:str, wordlist_path:str) -> None:
     """Основной компонент"""
     if not os.path.exists(wordlist_path):
         sys.exit(f"{BOLD}{RED}wordlist not found: {wordlist_path}{RESET}")
+    if "://" not in url:
+        sys.exit(f"{BOLD}{RED}url not found: {url}{RESET}")
+    if url[-1] == "/":url = url[:-1]
 
     wordlist = get_wordlist(wordlist_path=wordlist_path)
+    path_file = url.split("://")[1]+".txt"
+    if os.path.exists(path_file):os.remove(path_file)
+
     print(f"| Длина списка: {len(wordlist)}")
 
     for count, word in enumerate(wordlist, start=1):
@@ -57,6 +67,7 @@ def fuzz_subdomains(url:str, wordlist_path:str) -> None:
         result = check_subdomain(subdomain=full_domain)
         output_text = f"| [{count}/{len(wordlist)}] {full_domain} {result}"
         if result[0]:
+            recording_subdomain(subdomain=full_domain, path_file=path_file)
             output_text = f"{GREEN}{output_text}{RESET}"
         else:
             output_text = f"{RED}{output_text}{RESET}"
@@ -69,8 +80,8 @@ if __name__ == "__main__":
         if len(params) == 2 and ("url=" in params[0] or "--url=" in params[0]) and \
                 ("http://" in params[0] or "https://" in params[0]) and \
                 ("--wordlist=" in params[1] or "wordlist=" in params[1]):
-            url = params[0].split("url=")[1]
             wordlist = params[1].split("wordlist=")[1]
+            url = params[0].split("url=")[1]
             fuzz_subdomains(url=url, wordlist_path=wordlist)
     
         else:
