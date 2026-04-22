39f78e220bebadbcc71b4e400b2ada1b346c3eb2
cyberwarn <maksim@webcadev.ru>
2026-04-22 08:23:43 +0400

    update menu and args

--- cyberscan/cli.py
+++ cyberscan/cli.py
@@ -32,11 +32,16 @@ def show_menu() -> str:
     text = f"""\
     |{divide_line()}
     |   {BOLD}{__project__}{RESET}
-    |   {GREEN}[1] {BOLD}Сканер плагинов WordPress: wp-plugins{RESET}
-    |   {GREEN}[2] {BOLD}Определить версию WordPress: wp-version{RESET}
+    |
+    |   {GREEN}[1] {BOLD}Поиск поддоменов: fuzz_subdomain{RESET}
+    |   {GREEN}[2] {BOLD}Поиск файлов/директорий: fuzz_dirs{RESET}
+    |
+    |   {BLUE}Social{RESET}
     |   {GREEN}[3] {BOLD}Поиск по username: username{RESET}
-    |   {GREEN}[4] {BOLD}Поиск поддоменов: fuzz_subdomains{RESET}
-    |   {GREEN}[5] {BOLD}Поиск файлов/директорий: fuzz_dirs{RESET}
+    |
+    |   {BLUE}CMS{RESET}
+    |   {GREEN}[4] {BOLD}Сканер плагинов WordPress: wp-plugins{RESET}
+    |   {GREEN}[5] {BOLD}Определить версию WordPress: wp-version{RESET}
     """
     text = textwrap.dedent(text)
     return text
@@ -49,24 +54,31 @@ def main():
         if len(params) == 0:
             """Работа через меню"""
             item = input(f"{show_menu()}| >>> ").strip()
-            """WP plugins"""
-            if "wp-plugins" in item or item == "1":
-                scanWordPressPlugins(url=input("| URL: ").strip())
             
-            elif "wp-version" in item or item == "2":
-                """WP version"""
-                scanWordPressVersion(url=input("| URL: ").strip())
-            
-            elif "username" in item or item == "3":
-                """Search by username"""
-                cyberwarnuser(username=input("| Username: ").strip())
-            
-            elif "fuzz_subdomains" in item or item == "4":
+            if "fuzz_subdomain" in item or item == "1":
                 """Search subdomains"""
                 fuzz_subdomains(
                         url=input("| URL: ").strip(),
                         wordlist=input("| Wordlist(path): ").strip()
                         )
+            
+            elif "username" in item or item == "3":
+                """Search by username"""
+                cyberwarnuser(username=input("| Username: ").strip())
+            
+            elif "wp-plugins" in item or item == "4":
+                """WP plugins"""
+                scanWordPressPlugins(url=input("| URL: ").strip())
+            
+            elif "wp-version" in item or item == "5":
+                """WP version"""
+                scanWordPressVersion(url=input("| URL: ").strip())
+            
+        elif len(params) == 3 and "fuzz_subdomain" in params[0] \
+                and ("https://" in params[1] or "http://" in params[1]):
+            url = params[1]
+            wordlist = params[2]
+            fuzz_subdomains(url=url, wordlist_path=wordlist)
 
         elif len(params) == 2 and "wp-plugins" in params[0] \
                 and ("https://" in params[1] or "http://" in params[1]):
@@ -78,11 +90,6 @@ def main():
             """Работа с параметрами"""
             url = params[1]
             scanWordPressVersion(url=url)
-        elif len(params) == 3 and "fuzz_subdomains" in params[0] \
-                and ("https://" in params[1] or "http://" in params[1]):
-            url = params[1]
-            wordlist = params[2]
-            fuzz_subdomains(url=url, wordlist_path=wordlist)
 
         else:
             sys.exit(CyberHelp().help_main_menu(doc=__doc__))
