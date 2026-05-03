e63ebcd1ce6ed570dccb88efb12b7c6d92c21ba3
cyberwarn <maksim@webcadev.ru>
2026-05-03 17:18:56 +0400

    update wordlist and create new module: get_links

--- cyberscan/cli.py
+++ cyberscan/cli.py
@@ -28,6 +28,7 @@ from cyberscan.core.helper import CyberHelp
 from cyberscan.social.cyberwarnuser import cyberwarnuser
 from cyberscan.fuzz.fuzz_subdomain import fuzz_subdomains
 from cyberscan.fuzz.fuzz_dirs import fuzz_dirs
+from cyberscan.fuzz.get_links import get_links
 
 def show_menu() -> str:
     text = f"""\
@@ -36,13 +37,14 @@ def show_menu() -> str:
     |
     |   {GREEN}[1] {BOLD}Поиск поддоменов: fuzz_subdomain{RESET}
     |   {GREEN}[2] {BOLD}Поиск файлов/директорий: fuzz_dirs{RESET}
+    |   {GREEN}[3] {BOLD}Поиск ссылок на странице: get_links{RESET}
     |
     |   {BLUE}Social{RESET}
-    |   {GREEN}[3] {BOLD}Поиск по username: username{RESET}
+    |   {GREEN}[4] {BOLD}Поиск по username: username{RESET}
     |
     |   {BLUE}CMS{RESET}
-    |   {GREEN}[4] {BOLD}Сканер плагинов WordPress: wp-plugins{RESET}
-    |   {GREEN}[5] {BOLD}Определить версию WordPress: wp-version{RESET}
+    |   {GREEN}[5] {BOLD}Сканер плагинов WordPress: wp-plugins{RESET}
+    |   {GREEN}[6] {BOLD}Определить версию WordPress: wp-version{RESET}
     """
     text = textwrap.dedent(text)
     return text
@@ -69,16 +71,19 @@ def main():
                         url=input("| URL: ").strip(),
                         wordlist_path=input("| Wordlist(path): ").strip()
                         )
+            elif "get_links" in item or item == "3":
+                """Get all links by page"""
+                get_links(url=input("| URL: ").strip())
             
-            elif "username" in item or item == "3":
+            elif "username" in item or item == "4":
                 """Search by username"""
                 cyberwarnuser(username=input("| Username: ").strip())
             
-            elif "wp-plugins" in item or item == "4":
+            elif "wp-plugins" in item or item == "5":
                 """WP plugins"""
                 scanWordPressPlugins(url=input("| URL: ").strip())
             
-            elif "wp-version" in item or item == "5":
+            elif "wp-version" in item or item == "6":
                 """WP version"""
                 scanWordPressVersion(url=input("| URL: ").strip())
             
@@ -98,6 +103,10 @@ def main():
             """Работа с параметрами"""
             url = params[1]
             scanWordPressVersion(url=url)
+        elif len(params) == 2 and "get_links" in params[0] \
+                and ("http://" in params[1] or "https://" in params[1]):
+            url = params[1]
+            get_links(url=url)
 
         else:
             sys.exit(CyberHelp().help_main_menu(doc=__doc__))
---
+++ cyberscan/fuzz/get_links.py
@@ -0,0 +1,73 @@
+"""
+Модуль: get links
+
+Пример использования:
+    python3 -m cyberscan --url=http://example.com
+    python3 -m cyberscan.fuzz.get_links --url=http://example.com
+"""
+import os
+import requests
+import sys
+from cyberscan.core.header import Headers
+from cyberscan.core.helper import CyberHelp
+from cyberscan.core.colors import RED, RESET, BLUE, GREEN, BOLD, YELLOW
+from cyberscan.core.core import greeting
+from bs4 import BeautifulSoup
+
+def recording_urls(file_name:str, urls:[]=None) -> None:
+    with open(file_name, "w") as file:
+        for url in urls:
+            file.write(f"{url}\n")
+
+def extract_links(response:str, url:str) -> list[str] | None:
+    list_links = []
+    try:
+        bs = BeautifulSoup(response, "lxml")
+        all_links = bs.find_all(["a", "link"])
+        if len(all_links) > 0:
+            for link in all_links:
+                if link != None:
+                    link = link.attrs["href"]
+                    if link[0] == "/":link = link[1:]
+                    if "http" not in link and "://" not in link:
+                        link = f"{url}/{link}"
+                    if link not in list_links:
+                        list_links.append(link)
+        return list_links
+    except Exception as err:
+        print(f"{RED}ERROR Extraction: {err}{RESET}")
+        return list_links
+
+def get_links(url:str) -> None:
+    if url[-1] == "/":url = url[:-1]
+    try:
+        headers = Headers().create_headers()
+        headers["Host"] = url.split("://")[1]
+
+        response = requests.get(url, headers=headers)
+        status_code = response.status_code
+        if response.status_code == 200:
+            links = extract_links(response=response.text, url=url)
+            if len(links) > 0:
+                file_name = f"{url.split("://")[1]}_links.txt"
+                recording_urls(urls=links, file_name=file_name)
+                for count, link in enumerate(links, start=1):
+                    print(f"| [{count}] {GREEN}{link}{RESET}")
+            else:
+                sys.exit(f"| {RED}Ссылки не обнаружены!{RESET}")
+
+    except KeyboardInterrupt:
+        sys.exit(f"| {RED}\nExit...{RESET}")
+    except requests.exceptions.ConnectionError:
+        sys.exit(f"| {RED}{url}: Connection Error{RESET}")
+
+if __name__ == "__main__":
+    print(greeting())
+    params = sys.argv[1:]
+    if len(params) > 0 and \
+            "url=" in params[0] and \
+            ("http://" in params[0] or "https://" in params[0]):
+        url = params[0].split("url=")[1]
+        get_links(url=url)
+    else:
+        sys.exit(CyberHelp().help_main_menu(doc=__doc__))
